9d53a1a2bb097e6fbd8458cbbdc296533be42b24
spindouken <mlc9181209@gmail.com>
2023-04-15 16:11:30 -0500

    update imports

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -5,8 +5,10 @@ listening on 0.0.0.0:5000
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+import uuid
 app = Flask(__name__)
 
 
